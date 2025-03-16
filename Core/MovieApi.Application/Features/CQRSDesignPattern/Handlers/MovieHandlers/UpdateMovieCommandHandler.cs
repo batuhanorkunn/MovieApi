0d9df3c610731fbf908e9d729e21adaf6234fa0e
batuhan <batuh@DESKTOP-HE2T69G>
2025-03-17 02:40:58 +0300

    --

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MoviesApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public UpdateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async void Handle(UpdateMovieCommand command)
+        {
+            var value =await _context.Movies.FindAsync(command.MovieId);
+            value.Rating = command.Rating;
+            value.Title = command.Title;    
+            value.Status = command.Status;
+            value.Description = command.Description;
+            value.CoverImageUrl = command.CoverImageUrl;
+            value.Duration = command.Duration;  
+            value.CreatedYear = command.CreatedYear;
+            value.ReleaseDate = command.ReleaseDate;    
+            await _context.SaveChangesAsync();
+        }
+    }
+}
