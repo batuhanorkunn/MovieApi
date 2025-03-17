57c7d90810e794512301d600c3612f69f9234e3c
batuhan <batuh@DESKTOP-HE2T69G>
2025-03-18 00:05:05 +0300

    18.03.2025

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Commands/CastCommands/RemoveCastCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands
+{
+    public class RemoveCastCommand : IRequest
+    {
+        public int CastId { get; set; }
+        public RemoveCastCommand(int castId)
+        {
+            CastId = castId;
+        }
+
+        
+        
+    }
+}
